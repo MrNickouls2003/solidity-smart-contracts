119fc72795dfb8589c62dbff9f224dd1d574927c
Nícolas Augusto Montovani <138739066+MrNickouls2003@users.noreply.github.com>
2024-10-02 01:17:22 -0300

    Add files via upload

---
+++ smart_contracts/PersonDataStorage.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+//Smart Contract que armazena nome, idade e nacionalidade de uma pessoa brasileira. 
+//Com os dados da pessoa com o nome Nicolas, 21 para a idade e true para nacionalidade brasileira
+
+pragma solidity ^0.8.0;
+
+contract PersonDataStorage {
+    //Declarando variaveis
+    string public name = "Nicolas"; //nome publico para todos
+    uint private age = 21; //dados da idade serao privados para todos somente o contrato podera ler
+    bool internal brazilian = true; //internal e o tipo padrao, so esse contrato ou contratos que herdarm desse contrato terao acesso ao conteudo dessa variavel
+}
